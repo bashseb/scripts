0bbf545563e1e5cc486f06548020cf1025d695bb
Sebastian E <sebastian.eiser@gmail.com>
2014-10-01 12:34:08 +0200

    newlines

--- recOe1.sh
+++ recOe1.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 URL=http://mp3stream3.apasf.apa.at:8000
+
 # CONFIGURATION: 
 TARGET_DIR=${HOME}/oe1
 WWW_DIR="/var/www/oe1" # must be owned by executing user
 WWW_BASE_URL=
 # e.g  WWW_BASE_URL="http://myserver.at/oe1"
+
 PROGNAME=$(basename $0)
 die () {
    echo "${PROGNAME}: ${1:-"Unknown Error, Abort."}" 1>&2
    exit 1
 }
-finish () {
-  echo "$0 Error occured, cleaning"
-  # TODO
-}
-trap finish ERR SIGINT
+
 set -o errexit
 set -o nounset
+
 USAGE="Usage: $0 <length in seconds> <name-string (e.g Pasticcio)>"
+
 if [ "$#" -lt 2 ]; then echo $USAGE; exit 1; fi
+
 mkdir -p "${TARGET_DIR}"
 dat=$(date  +%Y-%m-%d)
 datRec=$(date +%Y_%m_%d)
@@ -27,17 +28,22 @@ NAME="${2}-${dat}"
 LEN="${1}"
 re='^[0-9]+$'
 [[ $LEN =~ $re ]] || die "error: '$LEN' is not an integer"
+
 echo "recording  ${NAME}.mp3 in ${TARGET_DIR}"
 echo ""
 if [[ -s "${TARGET_DIR}/${NAME}.mp3" ]]; then
   die "$LINENO: ${TARGET_DIR}/${NAME}.mp3 exists already. "
 fi
+
 streamripper "${URL}" --quiet -l $LEN -m 20 -d "${TARGET_DIR}" \
   -a "${NAME}.mp3" -A -s -u "winamp" || die "$LINENO: streamripper error"
+  
 id3tool --set-artist="Oe1" --set-title="${NAME}" --set-year=`date +%Y` \
   --set-album="Radio"  "${TARGET_DIR}/${NAME}.mp3" || echo \
   "id3 tool failed (nevermind)"
+  
 chmod -w "${TARGET_DIR}/${NAME}.mp3" || echo "chmod failed (nevermind)"
+
 # grep abstract from oe1 page
 status=0
 wget --quiet "http://oe1.orf.at/programm" --convert-links \
@@ -58,6 +64,7 @@ else
     [[ ${PIPESTATUS[0]} -eq 0 ]] || echo "${PROGNAME}: $LINENO: problem generating text output"
   fi
 fi
+
 # move to www folder, post link in email (only if the two variables are set)
 if ! [ -z "$WWW_DIR" -o -z "$WWW_BASE_URL" ]; then
   cd "${WWW_DIR}"
